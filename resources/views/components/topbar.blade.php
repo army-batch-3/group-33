d3800ee4fd9e05965cd16df1aee3b196b4f07c22
Ortizano <lordortizano08@gmail.com>
2021-08-03 15:50:26 -0700

    add register and logout button

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -188,10 +188,15 @@
                     Activity Log
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
-                </a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-jet-dropdown-link href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-jet-dropdown-link>
+                </form>
             </div>
         </li>
 
--- resources/views/layouts/main-content.blade.php
+++ resources/views/layouts/main-content.blade.php
@@ -22,13 +22,13 @@
             <div id="content">
 
                 <!-- Topbar -->
-               @include("components.topbar")
+                @include("components.topbar")
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
                     @yield('content')
-               
+
 
                 </div>
                 <!-- /.container-fluid -->
@@ -37,7 +37,7 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-           @include("components.footer")
+            @include("components.footer")
             <!-- End of Footer -->
 
         </div>
@@ -51,24 +51,44 @@
         <i class="fas fa-angle-up"></i>
 
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{URL::asset('admin/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{URL::asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <!-- Logout Modal-->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-primary" href="login.html">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <!-- Core plugin JavaScript-->
-    <script src="{{URL::asset('admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+        <!-- Bootstrap core JavaScript-->
+        <script src="{{ URL::asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ URL::asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="{{URL::asset('admin/js/sb-admin-2.min.js')}}"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="{{ URL::asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
-    <!-- Page level plugins -->
-    <script src="{{URL::asset('admin/vendor/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{URL::asset('admin/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="{{ URL::asset('admin/js/sb-admin-2.min.js') }}"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="{{URL::asset('admin/js/demo/datatables-demo.js')}}"></script>
+        <!-- Page level plugins -->
+        <script src="{{ URL::asset('admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ URL::asset('admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
-    @yield("content-script")
+        <!-- Page level custom scripts -->
+        <script src="{{ URL::asset('admin/js/demo/datatables-demo.js') }}"></script>
+
+        @yield("content-script")
 
 </body>
 
