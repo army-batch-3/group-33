ab8194f146bb014ce7c76e434a1d8d2b30df68cf
x@!n†™²³ <janrayleenofe@yahoo.com>
2021-09-11 08:52:37 +0800

    remove timestamps on edit

--- resources/views/warehouse/index.blade.php
+++ resources/views/warehouse/index.blade.php
@@ -18,7 +18,7 @@
         });
 
 
-           
+
         $(document).ready(function() {
             var editor = new $.fn.dataTable.Editor({
                 ajax: "{{ route('storeWarehouse') }}",
@@ -56,20 +56,6 @@
                     label: "Contact Number",
                     name: "contact_number",
                     type: 'text'
-                },  {
-                    label: 'Created At:',
-                    name: 'created_at',
-                    type: "hidden",
-                    def: function() {
-                        return moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
-                    }
-                }, {
-                    label: 'Updated At:',
-                    name: 'updated_at',
-                    type: "hidden",
-                    def: function() {
-                        return moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
-                    }
                 }]
             });
 
