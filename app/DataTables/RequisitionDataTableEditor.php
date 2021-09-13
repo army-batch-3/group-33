0fdcabd57c076204f64152f36e64678cbffd3ae5
Jan Ray Nofe <janrayleenofe@yahoo.com>
2021-09-14 00:27:57 +0800

    add and refactor Requisition form

--- app/DataTables/RequisitionDataTableEditor.php
+++ app/DataTables/RequisitionDataTableEditor.php
@@ -2,17 +2,17 @@
 
 namespace App\DataTables;
 
-use App\Models\Assets;
+use DB;
 use App\User;
-use Illuminate\Validation\Rule;
-use App\Models\RequisitionItems;
+use App\Models\Requisition;
 use App\Models\Transportation;
 use Yajra\DataTables\DataTablesEditor;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Assets;
 
 class RequisitionDataTableEditor extends DataTablesEditor
 {
-    protected $model = RequisitionItems::class;
+    protected $model = Requisition::class;
 
     /**
      * Get create action validation rules.
@@ -22,12 +22,11 @@ class RequisitionDataTableEditor extends DataTablesEditor
     public function createRules()
     {
         return [
-            "quantity" => "required",
-            "supplier_id" => "required",
-            "warehouse_id" => "required",
-            "transportations_id" => "required",
-            "assets_id" => "required",
-            "status" => "required",
+            'transportation_id' => 'required',
+            'employee_id' => 'required',
+            'assets_id' => 'required',
+            'quantity' => 'required',
+            'status' => 'required',
         ];
     }
 
@@ -40,14 +39,12 @@ class RequisitionDataTableEditor extends DataTablesEditor
     public function editRules(Model $model)
     {
         return [
-            "quantity" => "sometimes",
-            "supplier_id" => "sometimes",
-            "warehouse_id" => "sometimes",
-            "transportations_id" => "sometimes",
-            "status" => "sometimes",
-            "assets_id" => "sometimes",
+            'transportation_id' => 'sometimes',
+            'employee_id' => 'sometimes',
+            'assets_id' => 'sometimes',
+            'quantity' => 'sometimes',
+            'status' => 'sometimes',
         ];
-    
     }
 
     /**
@@ -61,21 +58,46 @@ class RequisitionDataTableEditor extends DataTablesEditor
         return [];
     }
 
-    public function updating(Model $model, array $data) {
+    public function updating(Model $model, array $data)
+    {
         // dd($data);
-        $status = Transportation::find($data['transportations_id']);
+        $status = Transportation::find($data['transportation_id']);
         $asset = Assets::find($data['assets_id']);
         if ($data['status'] == "Approved") {
             $status->is_available = 0;
             $status->save();
-        } else if($data['status'] == "Closed"){
+        } else if ($data['status'] == "Closed") {
             $status->is_available = 1;
             $status->save();
-        }else if($data['status'] == "Received"){
-           // add asset quantity
-           $asset->number_of_stocks +=  (int)$data['quantity'];
-           $asset->save();
+        } else if ($data['status'] == "Received") {
+            // add asset quantity
+            $asset->number_of_stocks -=  (int)$data['quantity'];
+            $asset->save();
         }
         return $data;
+    }
+
+    public function created(Model $model, array $data)
+    {
+        $fix = $model
+            ->join("pa_employees", "pa_employees.id", "=", "pa_requisition.employee_id")
+            ->join("pa_transportations", "pa_transportations.id", "=", "pa_requisition.transportation_id")
+            ->join("pa_assets", "pa_assets.id", "=", "pa_requisition.assets_id")
+            ->select(
+                'pa_requisition.id',
+                'pa_requisition.quantity',
+                'pa_requisition.created_at',
+                'pa_requisition.updated_at',
+                'pa_requisition.status',
+                'pa_requisition.employee_id',
+                'pa_requisition.transportation_id',
+                'pa_requisition.assets_id',
+                DB::raw("CONCAT(first_name,' ',middle_name,' ',last_name) AS employee_name"),
+                'pa_assets.name as assetsdb',
+                'pa_transportations.type AS transportationdb'
+            )
+            ->get()->toBase();
+        // dd($fix);
+        return $fix[count($fix) - 1];
     }
 }
--- resources/views/requisition.blade.php
+++ resources/views/requisition.blade.php
@@ -3,8 +3,8 @@
     <div class="container" style="width:100%">
         <div class="col-xs-12">
             <div class="box-content">
-                <h1>Restocks Form</h1>
-                <table id="requesition-table" class="display compact responsive nowrap"></table>
+                <h1>Requisition Form</h1>
+                <table id="requisition-table" class="display compact responsive nowrap"></table>
             </div>
         </div>
     </div>
@@ -21,23 +21,19 @@
 
              var editor = new $.fn.dataTable.Editor({
                 ajax: "{{ route('storeRequisition') }}",
-                table: "#requesition-table",
+                table: "#requisition-table",
                 idSrc: 'id',
                 fields: [{
                     label: "id",
                     name: "id",
                     type: 'hidden'
                 }, {
-                    label: "Supplier",
-                    name: "supplier_id",
-                    type: 'select'
-                },{
-                    label: "Warehouse",
-                    name: "warehouse_id",
+                    label: "Employee",
+                    name: "employee_id",
                     type: 'select'
                 }, {
                     label: "Transportation",
-                    name: "transportations_id",
+                    name: "transportation_id",
                     type: 'select'
                 }, {
                     label: "Assets",
@@ -62,16 +58,10 @@
                 }]
             });
 
-            // $('#requesition-table').on('click', 'tbody td:not(:first-child)', function(e) {
-            //     editor.inline( this, {
-            //         buttons: { label: '&gt;', fn: function () { this.submit(); } }
-            //     } );
-            // });
-      
-            $('#requesition-table').DataTable({
+            var requisition = $('#requisition-table').DataTable({
                 dom: "<'clear'l>Bfrtip",
                 ajax: {
-                    url: "{{ route('getRequesition') }}",
+                    url: "{{ route('getRequisition') }}",
                     type: 'post'
                 },
                 columns: [{
@@ -80,23 +70,18 @@
                         width: "10px"
                     },
                     {
-                        data: 'supplierdb',
-                        editField: 'supplierdb',
-                        title: 'Supplier',
-                    },
-                    {
-                        data: 'warehousedb',
-                        editField: 'warehousedb',
-                        title: 'Warehouse'
+                        data: 'employee_name',
+                        editField: 'employee_id',
+                        title: 'Employee',
                     },
                     {
                         data: 'transportationdb',
-                        editField: 'transportationdb',
+                        editField: 'transportation_id',
                         title: 'Transportation'
                     },
                     {
                         data: 'assetsdb',
-                        editField: 'assetsdb',
+                        editField: 'assets_id',
                         title: 'Assets'
                     },
                     {
@@ -123,14 +108,96 @@
                         editor: editor
                     },
                     {
-                        extend: "edit",
+                        extend: "remove",
                         editor: editor
                     },
                     {
-                        extend: "remove",
-                        editor: editor
+                        extends: 'select_single',
+                        className: 'marginLeft',
+                        text: 'Pending',
+                        action: function ( e, dt, node, config ) {
+                            editor
+                                .edit( requisition.row( { selected: true } ).index(), false )
+                                .set( 'status', "Pending" )
+                                .submit();
+                        }
+                    },{
+                        extends: 'select_single',
+                        className: 'marginLeft',
+                        text: 'Approved',
+                        action: function ( e, dt, node, config ) {
+                            editor
+                                .edit( requisition.row( { selected: true } ).index(), false )
+                                .set( 'status', "Approved" )
+                                .submit();
+                        }
+                    },{
+                        extends: 'select_single',
+                        className: 'marginLeft',
+                        text: 'Shipped',
+                        action: function ( e, dt, node, config ) {
+                            editor
+                                .edit( requisition.row( { selected: true } ).index(), false )
+                                .set( 'status', "Shipped" )
+                                .submit();
+                        }
+                    },{
+                        extends: 'select_single',
+                        className: 'marginLeft',
+                        text: 'Dropped Off',
+                        action: function ( e, dt, node, config ) {
+                            editor
+                                .edit( requisition.row( { selected: true } ).index(), false )
+                                .set( 'status', "Dropped Off" )
+                                .submit();
+                        }
+                    },{
+                        extends: 'select_single',
+                        className: 'marginLeft',
+                        text: 'Received',
+                        action: function ( e, dt, node, config ) {
+                            editor
+                                .edit( requisition.row( { selected: true } ).index(), false )
+                                .set( 'status', "Received" )
+                                .submit();
+                        }
+                    },
+                    {
+                        extends: 'select_single',
+                        className: 'marginLeft',
+                        text: 'Closed',
+                        action: function ( e, dt, node, config ) {
+                            editor
+                                .edit( requisition.row( { selected: true } ).index(), false )
+                                .set( 'status', "Closed" )
+                                .submit();
+                        }
                     }
                 ]
+            });
+            editor.on( 'postSubmit', function ( e, type ) {
+                if(type.data[0].status == "Received" || type.data[0].status == "Closed"){
+                    requisition.buttons().disable();
+                    requisition.buttons(0).enable();
+                    requisition.buttons(1).enable();
+                    (btn_value_id=="Received" ? requisition.buttons(7).enable() : '')
+                }else{
+                    requisition.buttons().enable();
+                }
+            } );
+
+            requisition.on( 'select', function () {
+                btn_value_id = requisition.row( { selected: true } ).data().status;
+                console.log("btn", btn_value_id);
+                if(btn_value_id == "Received" || btn_value_id == "Closed"){
+                    requisition.buttons().disable();
+                    requisition.buttons(0).enable();
+                    requisition.buttons(1).enable();
+                    (btn_value_id=="Received" ? requisition.buttons(7).enable() : '')
+                }else{
+                    requisition.buttons().enable();
+                }
+
             });
         });
     </script>
