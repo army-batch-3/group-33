e3a3fb9c0f52cb19e642fdadee8032cc3726aebe
Jan Ray Nofe <janrayleenofe@yahoo.com>
2021-09-16 12:37:21 +0800

    migration

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('dashboard', [HomeController::class, 'dashboard_lists'])->name('dashboard_lists');
 
 
-    Route::group(['middleware' => 'can:Roles - Permission',  'middleware' => 'can:Manage Employee'], function () {
+    // Route::group(['middleware' => 'can:Roles - Permission',  'middleware' => 'can:Manage Employee'], function () {
         Route::get('/Manage-User', [AdminController::class, 'manageUser'])->name('manageUser');
         Route::post('Personal-Benefits-Store', [AdminController::class, 'storeBenefits'])->name('storeBenefits');
         Route::post('Personal-Employment-Store', [AdminController::class, 'storeEmployment'])->name('storeEmployment');
@@ -47,9 +47,9 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         Route::post('PersonalInfo-Get', [AdminController::class, 'getPersonalAllInfo'])->name('getPersonalAllInfo');
         Route::post('Personal-Info-Store', [AdminController::class, 'storePersonalinfo'])->name('storePersonalinfo');
         Route::post('Personal-Info-Get', [AdminController::class, 'getPersonalinfo'])->name('getPersonalinfo');
-    });
+    // });
 
-    Route::group(['middleware' => 'can:Roles - Permission',  'middleware' => 'can:Assign Roles'], function () {
+    // Route::group(['middleware' => 'can:Roles - Permission',  'middleware' => 'can:Assign Roles'], function () {
         Route::get('Manage-Roles-Permission', [AdminController::class, 'permission_role'])->name('permission_role');
         Route::post('Permission-Get', [AdminController::class, 'getPermission'])->name('getPermission');
         Route::post('Roles-Select-Get', [AdminController::class, 'getRolesAvailable'])->name('getRolesAvailable');
@@ -58,54 +58,54 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         Route::post('Roles-Store', [AdminController::class, 'storeRole'])->name('storeRole');
         Route::post('Roles-Get', [AdminController::class, 'getRoles'])->name('getRoles');
         Route::post('User-Get', [AdminController::class, 'getLogin'])->name('getLogin');
-    });
+    // });
 
     // Manage Supplier
-    Route::group(['middleware' => 'can:Manage Suppliers'], function () {
+    // Route::group(['middleware' => 'can:Manage Suppliers'], function () {
         Route::post('Supplier-Store', [SupplierController::class, 'storeSupplier'])->name('storeSupplier');
         Route::post('Supplier-Get', [SupplierController::class, 'getSupplier'])->name('getSupplier');
         Route::get('/Suppliers', [SupplierController::class, 'supplier'])->name('supplier');
-    });
+    // });
 
     // Manage Assets
-    Route::group(['middleware' => 'can:Manage Assets'], function () {
+    // Route::group(['middleware' => 'can:Manage Assets'], function () {
         Route::get('/Assets', [AssetController::class, 'index'])->name('assets');
         Route::post('/Assets-Get', [AssetController::class, 'getAssets'])->name('getAssets');
         Route::post('/Assets-Store', [AssetController::class, 'storeAssets'])->name('storeAssets');
-    });
+    // });
 
     // Manage Employee
-    Route::group(['middleware' => 'can:Manage Employee'], function () {
+    // Route::group(['middleware' => 'can:Manage Employee'], function () {
         Route::get('/Employee', [EmployeeController::class, 'index'])->name('employee');
         Route::post('/Employee-Get', [EmployeeController::class, 'getEmployee'])->name('getEmployee');
         Route::post('/Employee-Store', [EmployeeController::class, 'storeEmployee'])->name('storeEmployee');
-    });
+    // });
 
     // Manage Warehouse
-    Route::group(['middleware' => 'can:Manage Warehouses'], function () {
+    // Route::group(['middleware' => 'can:Manage Warehouses'], function () {
         Route::post('/Warehouse-Store', [WarehouseController::class, 'storeWarehouse'])->name('storeWarehouse');
         Route::post('Warehouse-Get', [WarehouseController::class, 'getWarehouse'])->name('getWarehouse');
         Route::get('/warehouse', [WarehouseController::class, 'warehouse'])->name('warehouse.view');
-    });
+    // });
 
     // Manage Requisition
-    Route::group(['middleware' => 'can:Manage Requisitions'], function () {
+    // Route::group(['middleware' => 'can:Manage Requisitions'], function () {
         Route::get('/Restock', [RestockController::class, 'index'])->name('restock');
         Route::post('/Restock-Get', [RestockController::class, 'getRestock'])->name('getRestock');
         Route::post('/Restock-Store', [RestockController::class, 'storeRestock'])->name('storeRestock');
-    });
+    // });
 
-    Route::group(['middleware' => 'can:Manage Restocks'], function () {
+    // Route::group(['middleware' => 'can:Manage Restocks'], function () {
         Route::get('/Requisition', [RequisitionController::class, 'index'])->name('requisition');
         Route::post('/Requisition-Get', [RequisitionController::class, 'getRequisition'])->name('getRequisition');
         Route::post('/Requisition-Store', [RequisitionController::class, 'storeRequisition'])->name('storeRequisition');
-    });
+    // });
 
-    Route::group(['middleware' => 'can:Manage Transportation'], function () {
+    // Route::group(['middleware' => 'can:Manage Transportation'], function () {
         Route::post('Transportation-Store', [TransportationController::class, 'storeTransportation'])->name('storeTransportation');
         Route::post('Transportation-Get', [TransportationController::class, 'getTransportation'])->name('getTransportation');
         Route::get('/Transportations', [TransportationController::class, 'transportation'])->name('transportation');
-    });
+    // });
 
 
     Route::post('Info-Store', [AdminController::class, 'infoUpdate'])->name('infoUpdate');
